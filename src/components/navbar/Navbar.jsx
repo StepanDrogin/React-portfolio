e03cfcd246d748c866aae9dd5e1cb6f9c55fa0c2
StepanDrogin <drogenko.stepan@yandex.ru>
2021-07-23 01:12:39 +0300

    finished navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,20 @@ import "../navbar/navbar.scss";
 
 export default function Navbar() {
     return (
-        <div className="container" >
+        <div className="container">
+        <div className="navbar">
+        <div className="navbar__left">
+        <a href="mailto:drogenko.stepan@yandex.ru" className="navbar__mail">drogenko.stepan@yandex.ru</a>
+        </div>
+        <div className="navbar__right">
+        {/* eslint-disable-next-line */}
+            <a href="#" className="navbar__right-item">About me</a>
+                    {/* eslint-disable-next-line */}
+            <a href="#" className="navbar__right-item">Work</a>
+                    {/* eslint-disable-next-line */}
+            <a href="#" className="navbar__right-item">Contact</a>
+        </div>
+        </div>
         </div>
     )
 };
