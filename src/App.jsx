bf37a0893aa43327f81a650b64229bbef422fd67
StepanDrogin <drogenko.stepan@yandex.ru>
2021-07-23 17:13:35 +0300

    added Intro component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
+import Intro from './components/intro/Intro';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div>
     <Navbar></Navbar>
+    <Intro></Intro>
     </div>
   );
 }
