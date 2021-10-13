ddbdc57f386d6307a8845a126094f787267c2aa7
StepanDrogin <drogenko.stepan@yandex.ru>
2021-10-13 20:44:52 +0300

    added text to h2,h3 and p

--- src/components/aboutHardSkills/HardSkills.jsx
+++ src/components/aboutHardSkills/HardSkills.jsx
@@ -4,9 +4,9 @@ export default function HardSkills() {
     return (
         <div className="container">
             <div className="hardskills">
-                <h3 className="hardskills__h3"></h3>
-                <h2 className="hardskills__h2"></h2>
-                <p className="hardskills__p"></p>
+                <h3 className="hardskills__h3">About me</h3>
+                <h2 className="hardskills__h2">Hard Skills <span className="ampersand">&</span> <br></br> Soft Skills</h2>
+                <p className="hardskills__p">I create successful responsive websites that are fast, easy to use, and built with best practices. The main area of my expertise is front-end development, HTML, CSS, JS, building small and medium web apps, custom plugins, features, animations, and coding interactive layouts.</p>
          </div>
           </div>
     )
