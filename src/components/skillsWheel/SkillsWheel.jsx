b721e483f4c01542ba63b7a1d3113c22bc2b23e0
StepanDrogin <drogenko.stepan@yandex.ru>
2021-10-18 20:48:26 +0300

    fixed some properties  for svg

--- src/components/skillsWheel/SkillsWheel.jsx
+++ src/components/skillsWheel/SkillsWheel.jsx
@@ -34,43 +34,43 @@ export default function SkillsWheel() {
            <img className="GitHub__logo" src={GitHub} alt="GitHub Logo"></img>
            <img className="Engish__logo" src={Engish} alt="Engish Logo"></img>
            <svg className="line__html" width="102" height="100" viewBox="0 0 102 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M20.0872 3.28163L2 98M20.2123 2H100" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M20.0872 3.28163L2 98M20.2123 2H100" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
                 <svg className="line__css" width="153" height="73" viewBox="0 0 153 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M70.2427 2.55564L2 71.0036M71.2398 2H151" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M70.2427 2.55564L2 71.0036M71.2398 2H151" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
                       <svg className="line__figma" width="174" height="44" viewBox="0 0 174 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M90.4947 2.45576L2 42M92.0559 2H172" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M90.4947 2.45576L2 42M92.0559 2H172" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
  <svg className="line__jquery" width="161" height="14" viewBox="0 0 161 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M58.8847 11.8972L2 2M60.1524 12H159" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M58.8847 11.8972L2 2M60.1524 12H159" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
     <svg className="line__react" width="196" height="44" viewBox="0 0 196 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M90.5018 41.5442L2 2M92.0631 42H194" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M90.5018 41.5442L2 2M92.0631 42H194" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
     <svg className="line__API" width="155" height="88" viewBox="0 0 155 88" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M50.4024 85.4993L2.00244 2M50.9998 86H153" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M50.4024 85.4993L2.00244 2M50.9998 86H153" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
     <svg className="line__AXIOS" width="3" height="121" viewBox="0 0 3 121" fill="none" xmlns="http://www.w3.org/2000/svg">
-<line x1="1.9" y1="119.9" x2="1.90001" y2="1.1" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round"/>
+<line x1="1.9" y1="119.9" x2="1.90001" y2="1.1" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round"/>
 </svg>
 <svg className="line__webpack" width="155" height="88" viewBox="0 0 155 88" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M104.597 85.4993L152.997 2M104 86H2" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M104.597 85.4993L152.997 2M104 86H2" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 <svg className="line__vue" width="195" height="44" viewBox="0 0 195 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M104.499 41.5443L193 2M103.937 42H2" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M104.499 41.5443L193 2M103.937 42H2" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 <svg className="line__BEM" width="160" height="14" viewBox="0 0 160 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M102.105 11.8954L158 2M100.838 12H2" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M102.105 11.8954L158 2M100.838 12H2" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 <svg className="line__javascript" width="174" height="44" viewBox="0 0 174 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M83.5053 2.45576L172 42M81.9441 2H2" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M83.5053 2.45576L172 42M81.9441 2H2" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 <svg className="line__github" width="153" height="73" viewBox="0 0 153 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M82.7573 2.55564L151 71.0036M81.7602 2H2" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M82.7573 2.55564L151 71.0036M81.7602 2H2" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 <svg className="line__english" width="102" height="100" viewBox="0 0 102 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M81.9128 3.28163L100 98M81.7876 2H2" stroke="#D2D2D2" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M81.9128 3.28163L100 98M81.7876 2H2" stroke="#D2D2D2" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 <h3 className="HTML__h3">HTML</h3>
 <h3 className="CSS__h3">CSS/SASS-SCSS/Bootstrap</h3>
