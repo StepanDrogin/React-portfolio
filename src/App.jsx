54dd1ceda5c301dd3f2a82a002819d9a793bb213
StepanDrogin <drogenko.stepan@yandex.ru>
2021-10-22 00:48:19 +0300

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.scss';
-import HardSkills from './components/aboutHardSkills/HardSkills';
-import SoftSkills from './components/aboutSoftSkills/SoftSkills';
-import Footer from './components/footer/Footer';
-import Intro from './components/intro/Intro';
-import MyWorks from './components/myWorks/MyWorks';
-import Navbar from './components/navbar/Navbar';
-import SkillsWheel from './components/skillsWheel/SkillsWheel';
+import Footer from "./components/footer/Footer";
+import HomePage from "./components/homePage/HomePage";
+import LargusPage from "./components/largusPage/LargusPage";
+import NailsPage from "./components/nailsPage/NailsPage";
+import Konstuct from "./components/konstuctPage/KonstuctPage";
+import WeatherPage from "./components/weatherPage/WeatherPage";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
+    <Router>
     <div>
     <Navbar></Navbar>
-    <Intro></Intro>
-    <HardSkills></HardSkills>
-    <SkillsWheel></SkillsWheel>
-    <SoftSkills></SoftSkills>
-    <MyWorks></MyWorks>
+    <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/largus">
+            <LargusPage />
+          </Route>
+          <Route path="/nails">
+            <NailsPage />
+          </Route>
+          <Route path="/konstuct">
+            <Konstuct />
+          </Route>
+          <Route path="/weather">
+            <WeatherPage />
+          </Route>
+        </Switch>
     <Footer></Footer>
     </div>
+  </Router>
   );
 }
 
