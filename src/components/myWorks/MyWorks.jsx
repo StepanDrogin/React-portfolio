770106cb6fc0bcad26e8f52ccf0c9456f264940c
StepanDrogin <drogenko.stepan@yandex.ru>
2021-10-22 00:49:49 +0300

    added router links

--- src/components/myWorks/MyWorks.jsx
+++ src/components/myWorks/MyWorks.jsx
@@ -1,8 +1,9 @@
 import "../myWorks/myworks.scss"
-import Konstruct from "../../assets/img/konstruct.png"
+import Konstuct from "../../assets/img/konstruct.png"
 import Largus from "../../assets/img/largus.png"
 import Nails from "../../assets/img/nails.png"
 import Weather from "../../assets/img/weather.png"
+import { Link } from "react-router-dom"
 
 export default function MyWorks() {
     return (
@@ -11,16 +12,24 @@ export default function MyWorks() {
             <h2 className="works__h2">My Works</h2>
             <div className="works__grid">
                 <div className="works__item-1">
+                        <Link to="/largus">
                     <img src={Largus} className="works__item-image" alt="Largus"></img>
+                        </Link>
                 </div>
                 <div className="works__item-2">
+                <Link to="/nails">
                     <img src={Nails} className="works__item-image" alt="Nails"></img>
+                    </Link>
                 </div>
                 <div className="works__item-3">
-                    <img src={Konstruct} className="works__item-image" alt="Konstruct"></img>
+                <Link to="/kontuct">
+                    <img src={Konstuct} className="works__item-image" alt="Konstuct"></img>
+                    </Link>
                 </div>
                 <div className="works__item-4">
+                <Link to="/weather">
                     <img src={Weather} className="works__item-image" alt="Weather"></img>
+                    </Link>
                 </div>
                 <div className="works__item-5">
                     <p className="works__p">I create successful responsive websites that are fast, easy to use, and built with best practices. The main area of my expertise is front-end development, HTML, CSS, JS, building small and medium web apps, custom plugins, features, animations, and coding interactive layouts.
@@ -28,7 +37,7 @@ export default function MyWorks() {
                     <div className="works__flex">
                     <p className="works__p">I create successful &#8594;</p>
                     {/* eslint-disable-next-line */}
-                    <a href="#" className="works__link">View More</a>
+                    <a href="https://github.com/StepanDrogin" target='_blank' className="works__link">View More</a>
                     </div>
                 </div>
             </div>
