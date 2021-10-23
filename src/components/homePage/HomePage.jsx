62b1a3f43a9ce9fa893c11787f079f83b46f3834
StepanDrogin <drogenko.stepan@yandex.ru>
2021-10-24 02:10:59 +0300

    added useEffect for scroll to top page

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -4,8 +4,12 @@ import '../homePage/homepage.scss';
 import Intro from '../intro/Intro';
 import MyWorks from '../myWorks/MyWorks';
 import SkillsWheel from '../skillsWheel/SkillsWheel';
+import { useEffect } from 'react';
 
 export default function HomePage() {
+    useEffect(() => {
+        window.scroll(0,0)
+    }, []);
     return(
         <div>
             <Intro></Intro>
