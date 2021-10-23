e1630db1e87ff86cc1274126a77aad1feb53d74f
StepanDrogin <drogenko.stepan@yandex.ru>
2021-10-24 02:12:12 +0300

    finished component and added useEffect

--- src/components/konstuctPage/KonstuctPage.jsx
+++ src/components/konstuctPage/KonstuctPage.jsx
@@ -1,8 +1,59 @@
 import '../konstuctPage/konstuctpage.scss';
+import HTML from '../../assets/svg/html-icon.svg'
+import CSS from '../../assets/svg/css-icon.svg'
+import SASS from '../../assets/svg/sass.svg'
+import KonstuctFrontImage from '../../assets/img/konstuct-front.png';
+import KonstuctBackImage from '../../assets/img/konstuct-back.png';
+import Nails from "../../assets/img/nails-small.png"
+import Largus from "../../assets/img/largus-small.png"
+import Weather from "../../assets/img/weather-small.png"
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function KonstuctPage() {
+    useEffect(() => {
+        window.scroll(0,0)
+    }, []);
     return (
         <div className='container'>
+        <h3 className='page__h3'>Konstuct</h3>
+        <div className='pages'>
+        <div className='page__left'>
+            <p className='page__p'>Long term project to replace ageing internal command-line system with a modern web app using React to consume API data. Client is a US-based retirement fund manager with over $10B in assets.
+The Employee Portal is used by employees
+of the company to manage customer accounts, transactions, and related information</p>
+        <div className='page__icons'>
+            <img src={HTML} className='page__icon' alt='html icon'></img>
+            <img src={CSS} className='page__icon' alt='css icon'></img>
+            <img src={SASS} className='page__icon' alt='vue icon'></img>
+        </div>
+        </div>
+        <div className='page__right'>
+            <img src={KonstuctBackImage} className='page__image-front konstuct-front' alt='Nails'></img>
+            <img src={KonstuctFrontImage} className='page__image-back konstuct-back' alt='Nails'></img>
+        </div>
+        </div>
+        <h2 className='page__h2'>More work</h2>
+        <div className='page__cards'>
+            <div className='page__card'>
+            <Link to="/nails">
+                <img src={Nails} className='page__card-img' alt='card'></img>
+                </Link>
+                <p className='card__name'>Nails</p>
+            </div>
+            <div className='page__card'>
+            <Link to='/largus'>
+                <img src={Largus} className='page__card-img' alt='card'></img>
+                </Link>
+                <p className='card__name'>Largus</p>
+            </div>
+            <div className='page__card'>
+            <Link to='/weather'>
+                <img src={Weather} className='page__card-img' alt='card'></img>
+                </Link>
+                <p className='card__name'>Weather</p>
+            </div>
+        </div>
         </div>
     )
 };
--- src/components/largusPage/LargusPage.jsx
+++ src/components/largusPage/LargusPage.jsx
@@ -4,40 +4,56 @@ import CSS from '../../assets/svg/css-icon.svg'
 import Vue from '../../assets/svg/vue-icon.svg'
 import LargusFrontImage from '../../assets/img/largus-front.png';
 import LargusBackImage from '../../assets/img/largus-back.png';
-import Konstuct from "../../assets/img/konstruct.png"
-import Nails from "../../assets/img/nails.png"
-import Weather from "../../assets/img/weather.png"
+import Konstuct from "../../assets/img/konstukt-small.png"
+import Nails from "../../assets/img/nails-small.png"
+import Weather from "../../assets/img/weather-small.png"
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function LargusPage() {
+    useEffect(() => {
+        window.scroll(0,0)
+    }, []);
     return (
         <div className='container'>
-        <h3 className='largus__h3'>Largus Stavropol</h3>
-        <div className='largus__left'>
-            <p className='largus__p'>Long term project to replace ageing internal command-line system with a modern web app using React to consume API data. Client is a US-based retirement fund manager with over $10B in assets.
+        <h3 className='page__h3'>Largus Stavropol</h3>
+        <div className='pages'>
+        <div className='page__left'>
+            <p className='page__p'>Long term project to replace ageing internal command-line system with a modern web app using React to consume API data. Client is a US-based retirement fund manager with over $10B in assets.
 The Employee Portal is used by employees
 of the company to manage customer accounts, transactions, and related information</p>
-        <div className='largus__icons'>
-            <img src={HTML} className='largus__icon' alt='html icon'></img>
-            <img src={CSS} className='largus__icon'alt='css icon'></img>
-            <img src={Vue} className='largus__icon'alt='vue icon'></img>
+        <div className='page__icons'>
+            <img src={HTML} className='page__icon' alt='html icon'></img>
+            <img src={CSS} className='page__icon' alt='css icon'></img>
+            <img src={Vue} className='page__icon' alt='vue icon'></img>
         </div>
         </div>
-        <div className='largus__right'>
-            <img src={LargusFrontImage} className='page_image' alt='largus'></img>
-            <img src={LargusBackImage} className='page_image' alt='largus'></img>
+        <div className='page__right'>
+            {/* eslint-disable-next-line */}
+            <a href='https://vk.com/feed' target='_blank'>
+            <img src={LargusFrontImage} className='page__image-front' alt='largus'></img>
+                <img src={LargusBackImage} className='page__image-back' alt='largus'></img>
+            </a>
+        </div>
         </div>
         <h2 className='page__h2'>More work</h2>
         <div className='page__cards'>
             <div className='page__card'>
+            <Link to="/kontuct">
                 <img src={Konstuct} className='page__card-img' alt='card'></img>
+                </Link>
                 <p className='card__name'>Konstuct</p>
             </div>
             <div className='page__card'>
+            <Link to='/nails'>
                 <img src={Nails} className='page__card-img' alt='card'></img>
+                </Link>
                 <p className='card__name'>Nails</p>
             </div>
             <div className='page__card'>
+            <Link to='/weather'>
                 <img src={Weather} className='page__card-img' alt='card'></img>
+                </Link>
                 <p className='card__name'>Weather</p>
             </div>
         </div>
