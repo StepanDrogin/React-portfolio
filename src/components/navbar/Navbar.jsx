c117c17b2392a2a222c25bdfa4bf22c2fe4d8b2a
StepanDrogin <drogenko.stepan@yandex.ru>
2021-10-26 07:43:21 +0300

    added burger menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "../navbar/navbar.scss";
+import { slide as Menu } from 'react-burger-menu';
 
 export default function Navbar() {
     return (
@@ -8,6 +9,18 @@ export default function Navbar() {
         <div className="navbar__left">
         <a href="mailto:drogenko.stepan@yandex.ru" className="navbar__mail">drogenko.stepan@yandex.ru</a>
         </div>
+        <Menu right noOverlay>
+        <Link to='/'>
+                {/* eslint-disable-next-line */}
+        <span href="#" className="navbar__right-item main__page">Main page</span>
+        </Link>
+        {/* eslint-disable-next-line */}
+            <a href="#about" className="navbar__right-item">About me</a>
+                    {/* eslint-disable-next-line */}
+            <a href="#work" className="navbar__right-item">My works</a>
+                    {/* eslint-disable-next-line */}
+            <a href="#footer" className="navbar__right-item">Contact</a>
+      </Menu>
         <div className="navbar__right">
                 <Link to='/'>
                 {/* eslint-disable-next-line */}
